bd011d14904d8634e5ee9de999117dd379013a61
TomKuper <adalekseev@edu.hse.ru>
2020-01-13 23:28:15 +1000

    add Mongoose

--- TempRobotBack/index.js
+++ TempRobotBack/index.js
@@ -1,46 +1,47 @@
-var express = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const app = express();
 app.use(express.json());
 app.set('port', (process.env.PORT || 3000));
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://dbadmin:Vl240651@ds361968.mlab.com:61968/heroku_90wdqz4n";
-var db;
+const url = "mongodb://dbadmin:Vl240651@ds361968.mlab.com:61968/heroku_90wdqz4n";
 
-MongoClient.connect(url, (err, client) =>
-{
-    if (err) {
-        throw err;
-    }
-    db = client.db('heroku_90wdqz4n').collection('data');
+mongoose.connect(url);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-    db.find().toArray((err, result) =>
-                                            {
-                                                if (err) {
-                                                    throw err;
-                                                }
-                                                console.log(result);
-                                            });
-});
+const Measurement = new Schema({
+                            date       : {type: Date, default: Date.now},
+                            temperature: Number,
+                            humidity   : Number,
+                            battery    : Number
+                        });
+
+const MeasurementModel = mongoose.model('Measurement', Measurement);
 
 app.get('/', (req, res) =>
 {
-    db.find().toArray((err, result) =>
-                      {
-                          if (err) {
-                              throw err;
-                          }
-                          res.send(result);
-                      });
+    MeasurementModel.find({}, (err, docs) =>
+    {
+        res.send(docs);
+    });
 });
 
 app.post('/', (req, res) =>
 {
     const body = req.body;
-    console.log(body);
-    db.insertOne({date:body.date,temperature:body.temperature,humidity:body.humidity,battery:body.battery}).then(result =>{
-        res.send(result.insertedId);
+    const instance = new MeasurementModel();
+    instance.date = new Date(body.date*1000);
+    instance.temperature = body.temperature;
+    instance.humidity = body.humidity;
+    instance.battery = body.battery;
+
+    instance.save().then(obj=>{
+       res.send(obj);
     });
 });
+
 
 app.listen(
     app.get('port'),
